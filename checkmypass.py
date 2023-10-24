03281c248a11fc51e2aeccaaf88f6298d962d463
Susana Chavez <susanachavez8@hotmail.com>
2023-10-24 11:04:49 -0500

    capitalize done

--- checkmypass.py
+++ checkmypass.py
@@ -36,7 +36,7 @@ def main(args):
                 f'{password} was found {count} times... you should probably change your password.')
         else:
             print(f'{password} was NOT found. Carry on!')
-        return 'Done you did it!'
+        return 'DONE you did it!'
 
 
 if __name__ == '__main__':
